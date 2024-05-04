fa47adad20a223c7c2edcee015ab6e4974f6caf6
miltondw <estradamilton2001@gmail.com>
2024-05-04 11:18:17 -0500

    iniciando, clase_7 routes and GET

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("hello word")
+const express = require('express')
+const app = express()
+const port = 3000
+
+const productos = [{
+    id: '1',
+    name: 'producto 1',
+    price: 2000},
+    {
+      id: '2',
+    name: 'producto 2',
+    price: 1500
+    },
+    {
+      id: '3',
+    name: 'producto 3',
+    price: 1550
+    }
+  ];
+  app.get('/products', (_, response) =>{
+    response.json(productos);
+  });
+  
+  app.get('/products/:id', (req, response) =>{
+    const { id } = req.params;
+    const productoId = productos.find(productoId => productoId.id === id);
+    if (productoId){
+      response.json(productoId);
+    }
+    else{
+      response.status(404).json({ error: "producto no encontrado"});
+    }
+  })
+  
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
