f34a8863743ecbbed2b07b7f337fb4ef84f00edf
miltondw <estradamilton2001@gmail.com>
2024-05-08 15:23:55 -0500

    cors *

--- api/index.js
+++ api/index.js
@@ -11,17 +11,18 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-const witheList = ['http://localhost:3000', 'http://localhost:5000'];
-const options = {
-  origin: (origin, callback) => {
-    if (witheList.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Error Cors'));
-    }
-  },
-};
-app.use(cors(options));
+// const witheList = ['http://localhost:3000', 'http://localhost:5000'];
+// const options = {
+//   origin: (origin, callback) => {
+//     if (witheList.includes(origin) || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Error Cors'));
+//     }
+//   },
+// };
+// app.use(cors(options));
+app.use(cors());
 
 app.get('/',(req,res)=>{
   res.send('Esto es una api de prueba en express')
