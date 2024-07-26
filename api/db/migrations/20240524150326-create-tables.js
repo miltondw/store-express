85b775b63c0b8bb9face5e89e8ac77265b83edb8
miltondw <estradamilton2001@gmail.com>
2024-07-26 17:31:42 -0500

    fix migrations

--- api/db/migrations/20240524150326-create-tables.js
+++ api/db/migrations/20240524150326-create-tables.js
@@ -3,6 +3,12 @@ const { UserSchema, USER_TABLE } = require('./../../components/user/user.model')
 const { ProductSchema, PRODUCT_TABLE } = require('./../../components/product/product.model');
 const { CategorySchema, CATEGORY_TABLE } = require('./../../components/category/category.model');
 const { CustomerSchema, CUSTOMER_TABLE } = require('./../../components/customer/customer.model');
+const { OrderSchema, ORDER_TABLE } = require('../../components/order/order.model'); 
+const {
+  OrderProductSchema,
+  ORDER_PRODUCT_TABLE,
+} = require('../../components/order/order-product.model');
+
 // * Comando para crear la migración
 // npm run migrations:generate create-tables
 module.exports = {
@@ -11,6 +17,8 @@ module.exports = {
     await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
     await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
     await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
+        await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+            await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
 
   },
 
@@ -19,6 +27,8 @@ module.exports = {
     await queryInterface.dropTable(PRODUCT_TABLE);
     await queryInterface.dropTable(CATEGORY_TABLE);
     await queryInterface.dropTable(CUSTOMER_TABLE);
+        await queryInterface.dropTable(ORDER_TABLE);
+            await queryInterface.dropTable(ORDER_PRODUCT_TABLE);
 
   },
 };
